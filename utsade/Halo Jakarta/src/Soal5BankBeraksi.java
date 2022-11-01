3e7c7cffcd5b9a54325f53b7b599b9bbe6346f45
adeefendiirawan <115549745+adeefendiirawan@users.noreply.github.com>
2022-11-01 03:46:14 -0700

    Add files via upload

---
+++ utsade/Halo Jakarta/src/Soal5BankBeraksi.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FTI UNSAP
+ */
+public class Soal5BankBeraksi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Soal5Bank bank = new Soal5Bank(100_000);
+        bank.getSaldo();
+        bank.simpanUang(500_000);
+        bank.ambilUang(800_000);
+        
+    }
+    
+}
+
